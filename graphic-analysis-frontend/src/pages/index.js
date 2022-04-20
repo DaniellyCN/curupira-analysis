41bf6bb2f0790dc4b2ca5fdd50b51b28dfa59e8a
Brandow Buenos <brandowbuenos@gmail.com>
2022-04-19 22:03:36 -0300

    feat: add flexy template

---
+++ graphic-analysis-frontend/src/pages/index.js
@@ -0,0 +1,25 @@
+import { Grid } from "@mui/material";
+import BlogCard from "../components/dashboard/BlogCard";
+import SalesOverview from "../components/dashboard/SalseOverview";
+import DailyActivity from "../components/dashboard/DailyActivity";
+import ProductPerfomance from "../components/dashboard/ProductPerfomance";
+
+export default function Index() {
+  return (
+    <Grid container spacing={0}>
+      <Grid item xs={12} lg={12}>
+        <SalesOverview />
+      </Grid>
+      {/* ------------------------- row 1 ------------------------- */}
+      <Grid item xs={12} lg={4}>
+        <DailyActivity />
+      </Grid>
+      <Grid item xs={12} lg={8}>
+        <ProductPerfomance />
+      </Grid>
+      <Grid item xs={12} lg={12}>
+        <BlogCard />
+      </Grid>
+    </Grid>
+  );
+}
