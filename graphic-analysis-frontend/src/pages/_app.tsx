7b49b0bcc56237dfff3db16b0b36aa591cb257de
Brandow Buenos <brandowbuenos@gmail.com>
2022-06-11 19:31:38 -0300

    feat: add page title

--- graphic-analysis-frontend/src/pages/_app.tsx
+++ graphic-analysis-frontend/src/pages/_app.tsx
@@ -1,15 +1,26 @@
 import "../../styles/globals.css";
-import type { AppProps } from "next/app";
+import AppProps from "next/app";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme/theme";
 import { CssBaseline } from "@mui/material";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../theme/createEmotionCache";
+import Head from "next/head";
+const clientSideEmotionCache = createEmotionCache();
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp(props: any) {
+  const { Component, pageProps, emotionCache = clientSideEmotionCache } = props;
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Cucupira Analysis</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
